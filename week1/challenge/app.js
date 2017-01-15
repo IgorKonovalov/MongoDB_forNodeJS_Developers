d2dc061211458954121129f6acf21457922c3508
Igor Konovalov <konovalov.avp@gmail.com>
2017-01-15 20:08:30 +0300

    post request working

--- week1/challenge/app.js
+++ week1/challenge/app.js
@@ -2,36 +2,42 @@ let express = require('express'),
     app = express(),
     engines = require('consolidate'),
     MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+    assert = require('assert'),
+    bodyParser = require('body-parser'); // нужно для передачи инфы из формы в js
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function errorHandler(err, req, res, next) {
+  console.error(err.message);
+  console.error(err.stack);
+  res.status(500).render('error_template', { error: err });
+}
 
 MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
 
-    assert.equal(null, err);
-    console.log("Successfully connected to MongoDB.");
+  assert.equal(null, err);
+  console.log("Successfully connected to MongoDB.");
 
-    app.get('/', function(req, res) {
-      
-    })
+  app.get('/', function(req, res, next) {
+    res.render('form');
+  })
 
-    app.get('/movies', function(req, res){
-
-        db.collection('movies').find({}).toArray(function(err, docs) { // получаем обьект из базы данных, превращаем в массив
-            res.render('movies', { 'movies': docs } ); // и пускаем на рендер. первое movies - название шаблона (см views), второе - то что мы в него пускаем
-        });
-
+  app.post('/movies', function(req, res){
+    db.collection('movies').find({}).toArray(function(err, docs) {
+      res.render('movies', { 'movies': docs } );
     });
+  });
 
-    app.use(function(req, res){
-        res.sendStatus(404);
-    });
+  app.use(function(req, res){
+    res.sendStatus(404);
+  });
 
-    let server = app.listen(3000, function() {
-        let port = server.address().port;
-        console.log('Express server listening on port %s.', port);
-    });
+  let server = app.listen(3000, function() {
+    let port = server.address().port;
+    console.log('Express server listening on port %s.', port);
+  });
 
 });
