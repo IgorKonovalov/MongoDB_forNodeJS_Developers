765af4f4df79a6fef565949abd2142914a043e96
Igor Konovalov <konovalov.avp@gmail.com>
2017-01-25 21:43:37 +0300

    code comments

--- week3/findAndCursorsInNodeJSDriver/app-explicitCursor.js
+++ week3/findAndCursorsInNodeJSDriver/app-explicitCursor.js
@@ -12,9 +12,9 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 
     var query = {"category_code": "biotech"};
 
-    var cursor = db.collection('companies').find(query);
+    var cursor = db.collection('companies').find(query); // мы можем присвоить результат поиска объекту, этот обьект - курсор, для которого работает форич. Таким образом объект cursor представляет из себя поток документа - метод find() не делает нового запроса к бд пока мы не вызываем следующий обьект, таким образом экономится память
 
-    cursor.forEach(
+    cursor.forEach( // это специальный форич для курсоров - принимает две функции, для текущего документа и ошибки
         function(doc) {
             console.log( doc.name + " is a " + doc.category_code + " company." );
         },
--- week3/findAndCursorsInNodeJSDriver/app.js
+++ week3/findAndCursorsInNodeJSDriver/app.js
@@ -1,22 +1,22 @@
 // mongoimport -d crunchbase -c companies companies.json
 
 var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+    assert = require('assert'); // простая проверка на ошибки
 
 
-MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
+MongoClient.connect('mongodb://localhost:27017/companies', function(err, db) { // 'куда', функция - обработчик ()
 
-    assert.equal(err, null);
+    assert.equal(err, null); // тут проверяем что нет ошибки на то что нет коннекта к базе
     console.log("Successfully connected to MongoDB.");
 
     var query = {"category_code": "biotech"};
 
-    db.collection('companies').find(query).toArray(function(err, docs) {
+    db.collection('companies').find(query).toArray(function(err, docs) { // представляем полученное в поиске как массив (ES6) - используется весь объем найденного сразу. Когда мы вызываем метод toArray - метод find вызывается для всего объекта сразу, потому что мы должны иметь весь объект для перевода его в массив целиком.
 
-        assert.equal(err, null);
-        assert.notEqual(docs.length, 0);
+        assert.equal(err, null); // опять выкидываем ошибку если не приконектились к коллекции
+        assert.notEqual(docs.length, 0); // или нет документов, удовлетворяющих запросу
 
-        docs.forEach(function(doc) {
+        docs.forEach(function(doc) { // если все хорошо, форичем выводим результат(у нас уже массив на руках)
             console.log( doc.name + " is a " + doc.category_code + " company." );
         });
 
