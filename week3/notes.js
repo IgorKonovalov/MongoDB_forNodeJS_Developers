8c4bd406ec663602209f32a8b1b7cadf00a30ccd
Igor Konovalov <konovalov.avp@gmail.com>
2017-01-29 18:07:24 +0300

    minor issues

--- week3/notes.js
+++ week3/notes.js
@@ -1,3 +1,5 @@
 db.dropDatabase() // удаляет текущую базу - выполняется ИЗ mongoshell
 mongoimport -d crunchbase -c companies companies.json // добавляет в базу 'crunchbase' в коллекцию 'companies' инфу из json - выполняется из командной строки!  (mongoimport отдельная прога)
 db.companies.createIndex({permalink: 1}) // создает индекс сортировки
+cursor.sort({query: 1}); // сортировка в прямом порядке - меньшее будет выведено раньше
+// порядок выполнения сортировки: sort -> skip -> limit
