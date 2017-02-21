1e007e196e09575592e2be533121d6689af2a76c
Igor Konovalov <konovalov.avp@gmail.com>
2017-02-21 08:22:42 +0300

    hw6-3 correct

--- week6_aggregation_framework/homework/6-3.js
+++ week6_aggregation_framework/homework/6-3.js
@@ -1,10 +1,10 @@
 // For companies in our collection founded in 2004 and having 5 or more rounds of funding, calculate the average amount raised in each round of funding. Which company meeting these criteria raised the smallest average amount of money per funding round? You do not need to distinguish between currencies. Write an aggregation query to answer this question.
 
-db.companies.aggregate([ // сверху будет компания, встречающаяся 2 раза в DB. после unwind количество ее funding rounds будет 8 вместо 4 и она встанет на первое место, поэтому берем второе
+db.companies.aggregate([ // INCORRECT - doesn't work with doubled companies in Db
   { $match: { "founded_year": 2004 }},
   { $unwind: "$funding_rounds" },
   { $group: {
-    _id: { company: "$name" },
+    _id: "$name",
     num_rounds: { $sum: 1 },
     funded: { $push: "$funding_rounds.raised_amount"}
   } },
@@ -12,6 +12,27 @@ db.companies.aggregate([ // сверху будет компания, встре
   { $project: {
     _id: 1,
     num_rounds: 1,
+    average_funding: {$avg: "$funded"}
+  }},
+  { $sort: {"average_funding": 1}}
+]).pretty()
+
+db.companies.aggregate([ // CORRECT
+  { $match: { "founded_year": 2004 }},
+  { $project: {
+    _id: 1,
+    name: "$name",
+    funding_rounds: "$funding_rounds",
+    num_rounds: { $size: "$funding_rounds"}
+  }},
+  { $match: { "num_rounds": {$gte: 5}}},
+  { $unwind: "$funding_rounds" },
+  { $group: {
+    _id: "$name",
+    funded: { $push: "$funding_rounds.raised_amount"}
+  } },
+  { $project: {
+    _id: 1,
     average_funding: {$avg: "$funded"}
   }},
   { $sort: {"average_funding": 1}}
