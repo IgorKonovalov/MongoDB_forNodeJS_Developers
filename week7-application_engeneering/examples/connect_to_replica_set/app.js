1eb3b373ab55ec2a58fd07dd1d7bdb97db023f04
Igor Konovalov <konovalov.avp@gmail.com>
2017-02-23 22:41:18 +0300

    week 7 notes and examples

---
+++ week7-application_engeneering/examples/connect_to_replica_set/app.js
@@ -0,0 +1,17 @@
+// first you need to establish a 3 instances of mongod and add them to replica set
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:30001,localhost:30002,localhost:30003/course", function(err, db) {
+    if (err) throw err;
+
+    db.collection("repl").insert({ 'x' : 1 }, function(err, doc) {
+        if (err) throw err;
+
+        db.collection("repl").findOne({ 'x' : 1 }, function(err, doc) {
+            if (err) throw err;
+
+            console.log(doc);
+            db.close();
+        });
+    });
+});
